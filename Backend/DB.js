cc66219215aa17b8abd4fa96505ac9e5c8aa12a5
Ấn Vương <vuongquocan2308@gmail.com>
2025-02-26 10:57:35 +0700

    adjust gitignore

--- Backend/DB.js
+++ Backend/DB.js
@@ -3,9 +3,9 @@ const { Pool } = pkg;
 
 const pool = new Pool({
   user: "postgres",
-  host: "172.30.3.66",
-  database: "postgres",
-  password: "cuoi08",
+  host: "localhost",
+  database: "EmployeeManagement",
+  password: "123456",
   port: 5432,
 });
 pool.connect();
