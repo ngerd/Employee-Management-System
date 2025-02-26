726a61b62b9958909b195023e40497156b23b79c
Your Name <you@example.com>
2025-02-26 10:49:12 +0700

    Login

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,24 +1,30 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Login from './page/Login';
+import CreateAccount from './page/CreateAccount';
 import AdminDashboard from './page/AdminDashboard';
 import Project from './page/Project';
 import CreateProject from './page/CreateProject';
 import Home from './page/Home';
 import ProjectDetail from './page/ProjectDetail';
+import Navbar from './component/Navbar';
+import Login from "./page/Login";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/admin-dashboard" replace />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/admin-dashboard" element={<AdminDashboard />} />
-        <Route path="/project" element={<Project />} />
-        <Route path="/create-project" element={<CreateProject />} />
-        <Route path="/project-detail" element={<ProjectDetail />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="min-h-screen bg-gray-100">
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/admin-dashboard" replace />} />
+          <Route path="/create-account" element={<CreateAccount />} />
+          <Route path="/admin-dashboard" element={<AdminDashboard />} />
+          <Route path="/project" element={<Project />} />
+          <Route path="/create-project" element={<CreateProject />} />
+          <Route path="/project-detail" element={<ProjectDetail />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
--- Frontend/src/page/Home.jsx
+++ Frontend/src/page/Home.jsx
@@ -6,7 +6,6 @@ import { FaGlobe } from "react-icons/fa";
 function Home() {
   return (
     <div className="min-h-screen bg-gray-100">
-      <Navbar />
       <div className="py-8 px-4 sm:px-8 lg:px-16">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-6">
           <Card
