697aff3872799a455fa5aaabd221ebe3b0ff0327
Truong Nguyen <93463792+ngerd@users.noreply.github.com>
2025-02-26 10:09:21 +0700

    upload Home page

--- Frontend/src/page/Home.jsx
+++ Frontend/src/page/Home.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../component/Navbar";
+import Card from "../component/Card";
+import { FaGlobe } from "react-icons/fa";
 
 function Home() {
   return (
-    <div>
-      Home
+    <div className="min-h-screen bg-gray-100">
+      <Navbar />
+      <div className="py-8 px-4 sm:px-8 lg:px-16">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-6">
+          <Card
+            Icon={FaGlobe}
+            title="Staff"
+            subtitle="Lorem ipsum dolor"
+            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, praesentium voluptatem omnis atque culpa repellendus."
+            linkText="See details"
+            href="#"
+          />
+          <Card
+            Icon={FaGlobe}
+            title="Project"
+            subtitle="Lorem ipsum dolor"
+            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, praesentium voluptatem omnis atque culpa repellendus."
+            linkText="See details"
+            href="#"
+          />
+          <Card
+            Icon={FaGlobe}
+            title="Timesheet"
+            subtitle="Lorem ipsum dolor"
+            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, praesentium voluptatem omnis atque culpa repellendus."
+            linkText="See details"
+            href="#"
+          />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
