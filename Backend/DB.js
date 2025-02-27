908e010093f1cde35a51243dc61775be331187bc
QuocAnVuong <vuongquocan2308@gmail.com>
2025-02-27 14:59:18 +0700

    Db connect

--- Backend/DB.js
+++ Backend/DB.js
@@ -3,12 +3,11 @@ const { Pool } = pkg;
 
 const pool = new Pool({
   user: "postgres",
-  host: "172.30.3.66",
+  host: "34.81.89.236",
   database: "postgres",
   password: "cuoi08",
-  port: 5432
+  port: 5432,
 });
 pool.connect();
 
 export default pool;
-  
