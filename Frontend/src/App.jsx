bf5ae78fb4f6ddc9b5be2a8addb7617ac3f236f8
Minh Nguyen <116651549+mattzuha@users.noreply.github.com>
2025-03-01 20:28:46 +0700

    AddTask

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -22,6 +22,7 @@ import ProjectTask from "./page/ProjectTask";
 import ProjectTeam from "./page/ProjectTeam";
 import Staff from "./page/Staff";
 import ContextProvider from "./context/ContextProvider";
+import AddTask from "./page/AddTask";
 
 function Layout() {
   const location = useLocation();
@@ -45,6 +46,8 @@ function Layout() {
           <Route path="/project-task" element={<ProjectTask />} />
           <Route path="/project-team" element={<ProjectTeam />} />
           <Route path="/staff" element={<Staff />} />
+          <Route path="/admin-dashboard" element={<AdminDashboard />} />
+          <Route path="/add-task" element={<AddTask />} />
         </Routes>
       </ContextProvider>
     </div>
--- Frontend/src/page/AddTask.jsx
+++ Frontend/src/page/AddTask.jsx
@@ -1,104 +1,96 @@
 import React, { useState } from "react";
-import TextInput from "../component/TextInput";
-import TextAreaInput from "../component/TextAreaInput";
-import Select from "react-select";
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import "primereact/resources/themes/lara-light-blue/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 
-const employees = [
-    { id: 1, name: "Alice Johnson" },
-    { id: 2, name: "Bob Smith" },
-    { id: 3, name: "Charlie Brown" },
-];
-
-const employeeOptions = employees.map(emp => ({ value: emp.id, label: emp.name }));
-
 const AddTask = () => {
-    const [taskName, setTaskName] = useState("");
-    const [taskDescription, setTaskDescription] = useState("");
-    const [selectedEmployees, setSelectedEmployees] = useState([]);
-    const [tasks, setTasks] = useState([]);
-    const [loading, setLoading] = useState(false);
+    const [formValues, setFormValues] = useState({
+        projectId: 1,
+        taskName: "",
+        taskDescription: "",
+        startDate: "",
+        dueDate: "",
+    });
 
-    const handleAddTask = () => {
-        if (taskName && selectedEmployees.length > 0) {
-            setTasks([...tasks, {
-                id: tasks.length + 1,
-                name: taskName,
-                description: taskDescription,
-                assignedTo: selectedEmployees
-            }]);
-            setTaskName("");
-            setTaskDescription("");
-            setSelectedEmployees([]);
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormValues({ ...formValues, [name]: value });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch("http://localhost:3000/task/add", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(formValues),
+            });
+            const data = await response.json();
+            console.log(data);
+        } catch (error) {
+            console.error("There was a problem adding a task to the project:", error);
         }
-    };
-
-    const handleDeleteTask = (id) => {
-        setTasks(tasks.filter(task => task.id !== id));
-    };
-
-    const assignedToTemplate = (rowData) => {
-        return rowData.assignedTo.map(emp => emp.label).join(", ");
     };
 
     return (
         <div className="mx-auto max-w-screen-xl px-6 py-12 sm:px-8 lg:px-10 grid grid-cols-1 lg:grid-cols-2 gap-6">
             <div className="rounded-lg bg-white p-8 shadow-lg">
                 <h2 className="text-2xl pb-8 font-extrabold text-gray-900">Add Task</h2>
-                <form className="space-y-4">
-                    <TextInput label="Task Name" id="taskName" name="task_name" value={taskName} onChange={(e) => setTaskName(e.target.value)} />
-                    <TextAreaInput label="Task Description (Optional)" id="taskDescription" name="task_description" value={taskDescription} onChange={(e) => setTaskDescription(e.target.value)} />
+                <form className="space-y-4" onSubmit={handleSubmit}>
+                    <input
+                        id="taskName"
+                        name="taskName"
+                        value={formValues.taskName}
+                        onChange={handleChange}
+                        className="mt-1 p-2 h-10 w-full rounded-md border-gray-300 bg-white text-sm text-gray-700 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 pr-10"
+                        placeholder="Task Name"
+                    />
 
-                    <div>
-                        <label className="font-medium">Assign To:</label>
-                        <Select
-                            options={employeeOptions}
-                            isMulti
-                            className="w-full mt-2"
-                            value={selectedEmployees}
-                            onChange={setSelectedEmployees}
+                    <textarea
+                        id="taskDescription"
+                        name="taskDescription"
+                        value={formValues.taskDescription}
+                        onChange={handleChange}
+                        className="mt-1 p-2 h-20 w-full rounded-md border-gray-300 bg-white text-sm text-gray-700 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                        placeholder="Task Description"
+                    />
+
+                    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+                        <input
+                            type="date"
+                            id="startDate"
+                            name="startDate"
+                            value={formValues.startDate}
+                            onChange={handleChange}
+                            className="mt-1 p-2 h-10 w-full rounded-md border-gray-300 bg-white text-sm text-gray-700 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                            placeholder="Start Date"
+                        />
+                        <input
+                            type="date"
+                            id="dueDate"
+                            name="dueDate"
+                            value={formValues.dueDate}
+                            onChange={handleChange}
+                            className="mt-1 p-2 h-10 w-full rounded-md border-gray-300 bg-white text-sm text-gray-700 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                            placeholder="Due Date"
                         />
                     </div>
 
-                    <button
-                        type="button"
-                        className="inline-block w-full rounded-lg bg-black px-5 py-3 font-medium text-white sm:w-auto"
-                        onClick={handleAddTask}
-                    >
-                        Add Task
-                    </button>
+                    <div className="mt-6 text-right">
+                        <button type="submit" className="inline-block w-full rounded-lg bg-black px-5 py-3 font-medium text-white sm:w-auto">
+                            Add task
+                        </button>
+                    </div>
                 </form>
-
-                <ul className="mt-6">
-                    {tasks.map((task) => (
-                        <li key={task.id} className="p-2 border-b flex justify-between items-center">
-                            <span>{task.name} - {task.description || "No description"} - Assigned to {task.assignedTo.map(emp => emp.label).join(", ")}</span>
-                            <button className="bg-red-500 text-white px-3 py-1 rounded-md" onClick={() => handleDeleteTask(task.id)}>Delete</button>
-                        </li>
-                    ))}
-                </ul>
             </div>
 
             <div className="rounded-lg bg-white p-8 shadow-lg">
                 <h2 className="text-2xl pb-8 font-extrabold text-gray-900">Task List</h2>
-                <DataTable
-                    value={tasks}
-                    paginator
-                    rows={5}
-                    loading={loading}
-                    dataKey="name"
-                    emptyMessage="No task found."
-                    showGridlines
-                    className="rounded-lg shadow-md border border-gray-300 mt-4 z-10"
-                >
-                    {/* <Column field="id" header="ID" style={{ minWidth: '5rem' }} /> */}
-                    <Column field="name" header="Task Name" style={{ minWidth: '10rem' }} />
-                    <Column field="assignedTo" header="Assigned To" body={assignedToTemplate} style={{ minWidth: '12rem' }} />
-                </DataTable>
+                {/* Add DataTable here if needed */}
             </div>
         </div>
     );
