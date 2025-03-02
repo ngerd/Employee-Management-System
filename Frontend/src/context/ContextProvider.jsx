fb3c819826f84ec77a0bde31084a3ce0bb83e33f
Truong Nguyen <93463792+ngerd@users.noreply.github.com>
2025-03-02 19:50:17 +0700

    add task context

--- Frontend/src/context/ContextProvider.jsx
+++ Frontend/src/context/ContextProvider.jsx
@@ -3,25 +3,37 @@ import { createContext, useState } from "react";
 
 export const Employee = createContext(null);
 export const ProjectContext = createContext(null);
+export const TaskContext = createContext(null);
 
 function ContextProvider({ children }) {
   const [employeeId, setEmployeeId] = useState(null);
   const [isadmin, setisadmin] = useState(false);
   const [projectId, setProjectId] = useState(null);
+  const [taskId, setTaskID] = useState(null);
 
   return (
-    <ProjectContext.Provider value={{ projectId, setProjectId }}>
-      <Employee.Provider
+    <TaskContext.Provider 
+    value={(
+      taskId, 
+      setTaskID)}>
+      <ProjectContext.Provider
         value={{
-          employeeId,
-          setEmployeeId,
-          isadmin,
-          setisadmin,
+          projectId,
+          setProjectId,
         }}
       >
-        {children}
-      </Employee.Provider>
-    </ProjectContext.Provider>
+        <Employee.Provider
+          value={{
+            employeeId,
+            setEmployeeId,
+            isadmin,
+            setisadmin,
+          }}
+        >
+          {children}
+        </Employee.Provider>
+      </ProjectContext.Provider>
+    </TaskContext.Provider>
   );
 }
 
