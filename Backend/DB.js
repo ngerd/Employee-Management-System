3f6ba0269d3d4edd44e391ecbab82e246db6f3e5
TrinhKhasake <10421023@student.vgu.edu.vn>
2025-03-13 13:31:40 +0700

    new cloud

--- Backend/DB.js
+++ Backend/DB.js
@@ -1,13 +1,20 @@
+import dotenv from "dotenv";
+dotenv.config({ path: "../.env" }); // Adjust the path if needed
+
 import pkg from "pg";
 const { Pool } = pkg;
 
+// Create a new Pool using the DATABASE_URL from the .env file
 const pool = new Pool({
-  user: "postgres",
-  host: "34.81.89.236",
-  database: "postgres",
-  password: "cuoi08",
-  port: 5432
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
-pool.connect();
+
+// Optional: Test the database connection on startup
+pool.connect().catch((err) => {
+  console.error("Database connection error:", err);
+});
 
 export default pool;
