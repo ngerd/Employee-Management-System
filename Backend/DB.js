0aecd3a2ec9e3e2178223bf449b328ce2ca73516
Truong Nguyen <93463792+ngerd@users.noreply.github.com>
2025-03-13 14:17:44 +0700

    update database connection

--- Backend/DB.js
+++ Backend/DB.js
@@ -2,11 +2,17 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 const pool = new Pool({
-  user: "postgres",
-  host: "34.81.89.236",
-  database: "postgres",
-  password: "cuoi08",
-  port: 5432
+  // user: "postgres",
+  // host: "34.81.89.236",
+  // database: "postgres",
+  // password: "cuoi08",
+  // port: 5432
+  user: "employeemanagement",
+  host: "dpg-cv8kh5dumphs73cptj4g-a.singapore-postgres.render.com",
+  database: "employeemanagement_uhfq",
+  password: "WsEylr9iInxF5CdS027LZGEnn2zf8BzP",
+  port: 5432,
+  ssl: { rejectUnauthorized: false }
 });
 pool.connect();
 
